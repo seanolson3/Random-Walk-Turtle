9064c6fef5afafab057477e5daf26a7d052e4275
Sean <103209263+seanolson3@users.noreply.github.com>
2023-07-24 16:53:10 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+import turtle as t
+import random
+
+# Create an instance, called timmy_the_turtle, of the class Turtle from the turtle module
+tim = Turtle()
+t.colormode(255)
+
+colors = ["DodgerBlue", "DodgerBlue1", "DodgerBlue2", "DodgerBlue3", "DodgerBlue4", "tomato", "tomato1", "tomato2", "tomato3", "tomato4"]
+headings = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+def random_color():
+  r = random.randint(0, 255)
+  g = random.randint(0, 255)
+  b = random.randint(0, 255)
+  random_color = (r, g, b)
+  return random_color
+
+for _ in range(500):
+  tim.color(random_color())
+  tim.forward(30)
+  tim.setheading(random.choice(headings))
